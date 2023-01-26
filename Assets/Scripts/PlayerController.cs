c259763c0fe16eb100559da21636e69652e5ab0c
mrcrutgers <123099825+mrcrutgers@users.noreply.github.com>
2023-01-26 15:46:34 -0500

    Text
    Display count and win message

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,22 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 
 public class PlayerController : MonoBehaviour
 {
 
     private Rigidbody rb;
+    private int count;
     private float movementX;
     private float movementY;
 
     public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+
     
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+
+        SetCountText();
+        winTextObject.SetActive(false);
         
     }
 
@@ -26,6 +35,16 @@ public class PlayerController : MonoBehaviour
         Vector2 movementVector = movementValue.Get<Vector2>();
         movementX = movementVector.x;
         movementY = movementVector.y;
+
+    }
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= 12)
+        {
+            winTextObject.SetActive(true);
+
+        }
 
     }
 
@@ -41,6 +60,9 @@ public class PlayerController : MonoBehaviour
         if(other.gameObject.CompareTag("PickUp")){
 
             other.gameObject.SetActive(false);
+            count = count + 1;
+
+            SetCountText();
         }
 
     }
